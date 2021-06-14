58cda3681a486a2c1b9a3a50005a52556135eb74
Fabien Rassinier <frassinier@talend.com>
2021-06-15 00:26:34 +0200

    feat: figma

--- stories/Hero.stories.js
+++ stories/Hero.stories.js
@@ -1,13 +1,13 @@
 import { Figma } from "storybook-addon-designs/blocks";
 
-export const Default = (props) => (
+export const Default = () => (
   <Figma
     url="https://www.figma.com/file/16DCGEgU1U6DArqH98I0Kv/DSF-x-Talend?node-id=324%3A177"
     collapsable={false}
     showLink={false}
   />
 );
-Default.parameters = { docs: { disable: true } };
+// Default.parameters = { docs: { disable: true } };
 
 export default {
   title: "Example/Hero",
