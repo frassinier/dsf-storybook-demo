aa735550908d326b7f6dde181e3d4980c5a8e115
Fabien Rassinier <frassinier@talend.com>
2021-06-15 16:30:48 +0200

    chore: reset

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   stories: [
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../stories/**/*.stories.mdx",
+    "../stories/Introduction.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)",
   ],
   addons: [
     "@storybook/addon-links",
