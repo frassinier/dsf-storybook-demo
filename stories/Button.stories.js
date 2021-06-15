febb860c404e1af7c9afbcae9cdbd78b745dde60
Fabien Rassinier <frassinier@talend.com>
2021-06-15 16:04:09 +0200

    chore: reset

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,12 +1,13 @@
-import React from 'react';
-
-import { Button } from './Button';
+import React from "react";
+import { screen } from "@testing-library/dom";
+import userEvent from "@testing-library/user-event";
+import { Button } from "./Button";
 
 export default {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
 };
 
@@ -15,22 +16,22 @@ const Template = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: "Button",
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  label: "Button",
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
+  size: "large",
+  label: "Button",
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+  size: "small",
+  label: "Button",
 };
